9584297e7b78304d6aa070325aa1a809c4e44657
Poorvi <poorvi.b26@gmail.com>
2025-08-29 18:58:40 -0700

    use the todo routes in server file

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
+const todoRoutes = require("./routes/todoRoutes.js")
+
+// use json middleware
+app.use(express.json());
+
+// mount todo routes
+app.use("/todos", todoRoutes)
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World!</h1>")
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 })
